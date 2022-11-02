ed063e62fc845214f0bce68891c3556f5b9e2ebf
Juan M Hernandez <juanhm93@gmail.com>
2022-11-02 16:44:50 -0500

    add archivo env

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import "@styles/global.scss";
 
 const App = () =>{
     return (
+        <div>
         <h1>
             Base de proyecto de futbol poco - React 
         </h1>
+        </div>
     )
 }
 
